9cb30066034082607bb7ee77042143fe28ef10a2
Mike Fluff <mikefluff@mikefluff.com>
2017-01-25 19:14:25 +0300

    Multisig contract refactoring

--- contracts/Managed.sol
+++ contracts/Managed.sol
@@ -1,27 +1,31 @@
 pragma solidity ^0.4.4;
 
 import "Configurable.sol";
+import "./zeppelin/ownership/Shareable.sol";
 
-contract Managed is Configurable {
-  enum Vote {accept, revoke, deploy}
+contract Managed is Configurable, Shareable {
   enum Operations {createLOC,editLOC}
-  Vote vote;
-  Operations operations;
   uint public numAuthorizedKeys;
   mapping(address => bool) public authorizedKeys;
-  mapping(bytes32 => PendingAction) internal pendingActions;
-  struct PendingAction {
-    string stringValue;
-    uint value;
-    address addr;
-    Setting settings;
-    Operations operation;
+  uint pendingTxsCount;
+  mapping (uint => bytes32) pendingTxs;
+  mapping (bytes32 => Transaction) txs;
+
+  struct Transaction {
+    bytes data;
+    Operations op;
+    bool initialized;
   }
 
   function Managed() {
     address owner  = msg.sender;
     authorizedKeys[owner] = true;
+    owners[numAuthorizedKeys] = uint(owner);
     numAuthorizedKeys++;
+  }
+
+  function setRequired(uint _required) {
+    required = _required; 
   }
 
   modifier onlyAuthorized() {
@@ -30,17 +34,37 @@ contract Managed is Configurable {
       }
   }
 
-  function isAuthorized(address key) returns(bool) {
-      if (authorizedKeys[key]){
-        return true;
+  modifier execute(Operations _type) {
+   bytes32 _r = sha3(msg.data, block.number);
+   if(txs[_r].initialized) {
+    if (!confirm(_r)) {
+      txs[_r].data = msg.data;
+      txs[_r].op = _type;
+      pendingTxs[pendingTxsCount] = _r;
+      pendingTxsCount++;
+    } 
+    else {
+      _;
+    }
+   }
+  }
+
+  function confirm(bytes32 _h) onlymanyowners(_h) returns (bool) {
+      if (!this.call(txs[_h].data)) {
+        throw;
       }
-      else
-        return false;
+      delete txs[_h];
+      return true;
+  }
+
+  function isAuthorized(address key) returns(bool) {
+      return authorizedKeys[key];
   }
 
   function addKey(address key) onlyAuthorized() {
     if (!authorizedKeys[key]) { // Make sure that the key being submitted isn't already CBE.
       authorizedKeys[key] = true;
+      owners[numAuthorizedKeys] = uint(key);
       numAuthorizedKeys++;
     }
   }
